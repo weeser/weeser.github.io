0736bb455c0b3d1ac202258b4f8ebae87880effe
weeser <weeser@ksu.edu>
2024-03-16 23:52:45 -0500

    remove nuxt cache for now

--- .nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('A:/workspace/weeser.github.io/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
-  const parseContent: typeof import('A:/workspace/weeser.github.io/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
