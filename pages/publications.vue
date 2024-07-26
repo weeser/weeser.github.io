e53b4e563785e15b4c8c4feabe309913bcbc9d17
Josh Weese <weeser@ksu.edu>
2024-07-25 22:15:01 -0500

    fixing publications

--- pages/publications.vue
+++ pages/publications.vue
@@ -181,7 +181,8 @@ const onSortChange = (event) => {
                       <span v-if="entry.YEAR" class="year">{{ entry.YEAR }}.&nbsp</span>
                       <span v-if="entry.TITLE" class="title">
                         <NuxtLink class="text-primary-800 dark:text-primary-emphasis hover:underline" v-if="entry.DOI"
-                          :to="'https://doi.org/' + entry.DOI" target="_blank">{{ entry.TITLE }}</NuxtLink>.&nbsp
+                          :to="'https://doi.org/' + entry.DOI" target="_blank">{{ entry.TITLE }}</NuxtLink>
+                          <span v-else>{{ entry.TITLE }}</span>.&nbsp
                       </span>
                       <span v-if="entry.BOOKTITLE" class="book_title">{{ entry.BOOKTITLE }}<span v-if="entry.SERIES" class="series">&nbsp({{ entry.SERIES }})</span>.&nbsp</span>
                       <span v-if="entry.JOURNAL" class="journal"><em>{{ entry.JOURNAL }}.&nbsp</em></span>
