f269bb85d9e6301491e623bb6529e9700dd8bc6f
Josh Weese <weeser@ksu.edu>
2026-05-12 09:47:17 -0500

    updated student project list, added description for some

--- components/cv/StudentProjects.vue
+++ components/cv/StudentProjects.vue
@@ -1,8 +1,13 @@
 <template>
 	<PrimePanel header="Student Projects" toggleable>
 		<PrimeDataTable :value="ugProjects" class="px-12" size="small" tableStyle="min-width: 25rem" stripedRows>
-			<PrimeColumn field="students" header="Student Names" style="width: 40%;" sortable></PrimeColumn>			
-			<PrimeColumn field="project" header="Project" sortable ></PrimeColumn>
+			<PrimeColumn field="students" header="Student Names" style="width: 40%;" sortable></PrimeColumn>
+			<PrimeColumn field="project" header="Project" sortable>
+				<template #body="slotProps">
+					<div>{{ slotProps.data.project }}</div>
+					<small v-if="slotProps.data.description" class="project-description">{{ slotProps.data.description }}</small>
+				</template>
+			</PrimeColumn>
 			<PrimeColumn field="term" header="Term" sortable></PrimeColumn>
 		</PrimeDataTable>
 	</PrimePanel>
@@ -13,5 +18,10 @@ defineProps(['ugProjects'])
 </script>
 
 <style scoped>
-/* Add your component-specific styles here */
+.project-description {
+	display: block;
+	margin-top: 0.25rem;
+	color: var(--text-color-secondary);
+	line-height: 1.35;
+}
 </style>
